1b791bfcde308232c36fde3584dd017622c31472
Daniel Shin <daniel.shin1104@gmail.com>
2018-05-28 18:36:28 +0900

    Improve javascript API

--- www/detectmocklocation.js
+++ www/detectmocklocation.js
@@ -1,5 +1,15 @@
 var exec = require("cordova/exec");
 
-exports.check = function(success, error) {
-  exec(success, error, "DetectMockLocation", "check", []);
+/* success responses */
+var MOCKED = "mocked";
+var NOT_MOCKED = "not_mocked";
+
+exports.check = function(win, fail) {
+  var success = function(res) {
+    if (res === MOCKED) win(true);
+    else if (res === NOT_MOCKED) win(false);
+    else fail("Unknown success response: " + res);
+  }
+
+  exec(success, fail, "DetectMockLocation", "check", []);
 };
